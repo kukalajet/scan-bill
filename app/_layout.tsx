bd6448e020e49bcc66614d3abd34d39c3447d4cc
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2024-02-24 11:11:28 +0100

    Add "scan" screen to navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -47,6 +47,7 @@ function RootLayoutNav() {
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      <Stack.Screen name="scan" options={{ headerShown: false }} />
     </Stack>
   );
 }
