4e13d262890424ebc3bbaf0b322da88c06219891
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2024-02-24 09:54:12 +0100

    Add Animated view and Button to TabTwoScreen

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,9 +1,24 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 
 export default function TabTwoScreen() {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
     </View>
   );
 }
